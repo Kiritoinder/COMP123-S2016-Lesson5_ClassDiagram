208e21693bf6d513d72c43f0dd993eab74f325fd
Kiritoinder <inderbharaj@ymail.com>
2016-06-07 18:52:29 -0400

    Add project files.

---
+++ COMP213-S2016-Lesson5/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COMP213_S2016_Lesson5
+{
+    /**
+     * this class is the "driver" class for our program
+     */
+    public class Program
+    {
+        /**
+         * the main method for driver class
+         * 
+         * @method main
+         * @parm {string[]} args
+         */
+
+        public static void Main(string[] args)
+        {
+            //create instance of student
+            Student Inder = new Student("Inder", 18, "300874118");
+
+            Inder._sayHello();
+
+            Console.WriteLine();
+            Inder.Studies();
+
+
+            Inder.Courses.Add(new Course("comp123", "programming 2"));
+            Inder.ShowCourses();
+            Console.WriteLine();
+
+
+            //Create instance of teacher class
+            Teacher tom = new Teacher("Tom", 47, "123456789");
+            tom._sayHello();
+            Console.WriteLine();
+            tom.Teaches();
+
+            List<string> names = new List<string>();
+            names.Add("Inder");
+            names.Add("Abhi");
+
+            int count = 0;
+            Console.WriteLine();
+            Console.WriteLine("+++++++++++++++++++++++++++++++++++++++");
+            foreach (string name in names)
+            {
+                Console.WriteLine("name #" + count + " is " + name);
+                count++;
+            }
+            Console.WriteLine("+++++++++++++++++++++++++++++++++++++++");
+
+            List<Card> Deck = new List<Card>();
+
+
+            CreateDeck(Deck);
+        }
+
+        public static void CreateDeck(List<Card> deck)
+        {
+            string suit = "";
+            for (int i = 0; i < 4; i++)
+            {
+                switch (i)
+                {
+                    case 0:
+                        suit = "hearts";
+                        break;
+                    case 1:
+                        suit = "clubs";
+                        break;
+                    case 2:
+                        suit = "diamonds";
+                        break;
+                    case 3:
+                        suit = "spades";
+                        break;
+
+
+                }
+
+
+                for (int face = 1; face < 14; face++)
+                {
+                    deck.Add(new Card(face, suit));
+                }
+            }
+
+
+            /**       MyList myList = new MyList();
+                   myList.Add(1);
+                   myList.Add(5);
+                   myList.Clear();
+                   myList.Print();  */
+        }
+    }
+}
+
---
+++ COMP213-S2016-Lesson5/Teacher.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COMP213_S2016_Lesson5
+{
+    /**
+     *<summary>
+     * The teacher class is derived from person class
+     *  
+     * </summary> 
+     * @class teacher
+     * @extends person
+     */
+    class Teacher : Person
+    {
+        //PRIVATE INSTANCE VARIABLES ++++++++++++++++++++++++
+        private string _employeeID;
+
+        //PUBLIC PROPERTIES +++++++++++++++++++++++++++++++++
+        public string EmployeeID
+        {
+            get
+            {
+                return this._employeeID;
+            }
+            set
+            {
+                this._employeeID = value;
+            }
+        }
+        //CONSTRUCTOR
+
+        /**
+         * <summary>
+         * This constructor method takes 3 paramaters: name, age, emloyeeID             * 
+         * </summary>
+         * 
+         * @constructor Teacher
+         * @pram {string} name
+         * @pram {int} age
+         * @pram {string] employeeID
+         * 
+         */
+        public Teacher(string name, int age, string employeeID)
+            : base(name, age)
+        {
+            this.EmployeeID = employeeID;
+        }
+
+        // PUBLIC METHODS +++++++++++++++++++++++++++++++++++++++
+        /**
+         * <summary>
+         * This method enables the teacher behaviours for the teacher class
+         * </summary>
+         * 
+         * 
+         * 
+         */
+        public void Teaches()
+        {
+            Console.WriteLine(this.Name + " teaches.");
+        }
+    }
+}
